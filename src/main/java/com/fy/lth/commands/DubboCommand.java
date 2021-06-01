4522ab0c7c24a0b74526be0f4df01aff2d839e2e
fy249085 <fy249085@gongdao.com>
2021-06-01 17:32:04 +0800

    DubboCommand

---
+++ src/main/java/com/fy/lth/commands/DubboCommand.java
@@ -0,0 +1,34 @@
+package com.fy.lth.commands;
+
+import com.fy.lth.common.ChinesSizeConstraints;
+import com.fy.lth.common.util.TableUtil;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.CellMatchers;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
+import org.springframework.shell.table.TableModelBuilder;
+
+/**
+ * @author fengyue
+ * @date 2021/6/1
+ */
+@ShellComponent
+@ShellCommandGroup(value = "dubbo")
+public class DubboCommand {
+
+    @ShellMethod(value = "数据库日志配置", key = {"dubbo_ls"})
+    public Table dubbo_ls() {
+        TableModelBuilder builder = TableUtil.create(null);
+        builder.addRow().addValue("telnet localhost 20880").addValue("查看本机dubbo服务");
+
+        TableModel tableModel = builder.build();
+        TableBuilder tableBuilder = new TableBuilder(tableModel);
+        tableBuilder.on(CellMatchers.table()).addSizer(new ChinesSizeConstraints()).and();
+        return tableBuilder.addFullBorder(BorderStyle.fancy_light).build();
+    }
+
+}
