24f0f292bc5cb949ab49cd10420960cadd6f76d6
fy249085 <fy249085@gongdao.com>
2021-11-09 14:47:40 +0800

    docker

---
+++ src/main/java/com/fy/lth/commands/DockerCommand.java
@@ -0,0 +1,40 @@
+package com.fy.lth.commands;
+
+import com.fy.lth.common.ChinesSizeConstraints;
+import com.fy.lth.common.util.TableUtil;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.CellMatchers;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
+import org.springframework.shell.table.TableModelBuilder;
+
+/**
+ * @author fengyue
+ * @date 2021/11/9
+ */
+@ShellComponent
+@ShellCommandGroup(value = "docker")
+public class DockerCommand {
+
+    @ShellMethod(value = "docker", key = {"docker"})
+    public Table docker() {
+        TableModelBuilder builder = TableUtil.create(null);
+        builder.addRow().addValue("").addValue("paka/paka_mysql 为容器的name");
+        builder.addRow().addValue("docker logs -f --tail=1000 paka").addValue("实时查看指定容器的最新1000行日志");
+        builder.addRow().addValue("docker exec -it paka bash").addValue("以bash方式进入容器控制台");
+        builder.addRow().addValue("docker exec -it paka_mysql sh -c \"mysql -uroot -pZD71SEx82jg=\" ").addValue("进入mysql容器");
+        builder.addRow().addValue("docker logs | grep \"XXX\" paka").addValue("过滤容器的日志");
+        builder.addRow().addValue("docker logs paka | grep -A n  \"XXX\"").addValue("过滤容器的日志并显示匹配后和它后面的n行。");
+        builder.addRow().addValue("docker logs paka | grep -A n  \"XXX\"").addValue("过滤容器的日志并显示匹配后和它前面的n行。");
+        builder.addRow().addValue("docker logs paka | grep -A n  \"XXX\"").addValue("过滤容器的日志并显示匹配后和它前后的n行。");
+
+        TableModel tableModel = builder.build();
+        TableBuilder tableBuilder = new TableBuilder(tableModel);
+        tableBuilder.on(CellMatchers.table()).addSizer(new ChinesSizeConstraints()).and();
+        return tableBuilder.addFullBorder(BorderStyle.fancy_light).build();
+    }
+}
--- src/main/java/com/fy/lth/commands/RedisCommand.java
+++ src/main/java/com/fy/lth/commands/RedisCommand.java
@@ -25,6 +25,7 @@ public class RedisCommand {
         TableModelBuilder builder = TableUtil.create(null);
         builder.addRow().addValue("redis-cli -h 127.0.0.1 -p 6379 --intrinsic-latency 60").addValue("查看当前redis实例60秒内最大的响应延迟。");
         builder.addRow().addValue("redis-cli -h 127.0.0.1 -p 6379 --latency-history -i 1").addValue("查看当前redis实例一段时间内最大，最小，平均访问延迟。每隔1秒采样 redis");
+        builder.addRow().addValue("re di cli - h 127.0.0.1 - p 7001 -bigkeys  -i 0.1").addValue("定位大 key，指令每隔 100条 scan 指令就会休眠 O.ls, ops 就不会剧烈抬升，但是扫描的时间会变长。");
 
         TableModel tableModel = builder.build();
         TableBuilder tableBuilder = new TableBuilder(tableModel);
